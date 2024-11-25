cbbcf25f7f3ada492af3a126cd2e3e97d020ffb0
HARRY0133 <excuseboy13@gmail.com>
2024-11-25 20:29:08 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "Free bot"
-BOT_TOKEN = "6755775439:AAGkahjp3xK71u-jG6V0uQUR-xJgqLPt9yw"
-API_ID = 28328736
-API_HASH = "802254a44896baa87f3083b7af36b2e5"
+BOT_NAME = "rowdy123uploaderbot"
+BOT_TOKEN = "8195852910:AAGhCD8DxebQunQiGnkLyq8M_HXIQutiqug"
+API_ID = 27679306
+API_HASH = "d0f390f798eec4ddb9246a00a44b7a3f"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
