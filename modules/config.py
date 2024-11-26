14495953372ed6686cf8b69052b43d724c48a7bf
HARRY0133 <excuseboy13@gmail.com>
2024-11-26 19:17:19 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "rowdytxtbot"
-BOT_TOKEN = "7426490384:AAFR2QrUVyTPICFsA75XA17NdGE5HuRggeo"
+BOT_NAME = "cobratxtbot"
+BOT_TOKEN = "7606517173:AAGo-e-idfoSDyzNrc9FP6MkP9EKhIECnlo"
 API_ID = 27679306
 API_HASH = "d0f390f798eec4ddb9246a00a44b7a3f"
 MONGO_URI = "mongodb+srv://rowdybro013:DmnZ3s14aRbfA8tb@cluster01.ramjm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01"
